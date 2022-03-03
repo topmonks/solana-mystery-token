0aeff7738e9c0e38992b2022cc3dd55473626fe7
Toňa <antonina.nesmelova@topmonks.com>
2022-03-03 13:51:20 +0100

    Add wallet connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,105 @@
 import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://localhost:4001";
 
-function App() {
-  const [response, setResponse] = useState("");
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import * as anchor from "@project-serum/anchor";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  getPhantomWallet,
+  getSlopeWallet,
+  getSolflareWallet,
+  getSolflareWebWallet,
+  getSolletWallet,
+  getSolletExtensionWallet,
+  getSolongWallet,
+  getLedgerWallet,
+  getSafePalWallet,
+} from "@solana/wallet-adapter-wallets";
 
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("FromAPI", data => {
-      setResponse(data);
-    });
-  }, []);
+import {
+  WalletModalProvider
+} from '@solana/wallet-adapter-react-ui';
+
+import "./App.css";
+import Home from "./Home";
+
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
+
+const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
+const connection = new anchor.web3.Connection(rpcHost);
+
+const txTimeout = 30000; // milliseconds (confirm this works for your project)
+
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+  },
+  overrides: {
+    MuiButtonBase: {
+      root: {
+        justifyContent: 'flex-start',
+      },
+    },
+    MuiButton: {
+      root: {
+        textTransform: undefined,
+        padding: '12px 16px',
+      },
+      startIcon: {
+        marginRight: 8,
+      },
+      endIcon: {
+        marginLeft: 8,
+      },
+    },
+  },
+});
+
+const App = () => {
+  // Custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), []);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = useMemo(
+      () => [
+        getPhantomWallet(),
+        getSlopeWallet(),
+        getSolflareWallet(),
+        getSolflareWebWallet(),
+        getSolletWallet({ network }),
+        getSolletExtensionWallet({ network }),
+        getSolongWallet(),
+        getLedgerWallet(),
+        getSafePalWallet(),
+      ],
+      []
+  );
 
   return (
-      <p>
-        It's <time dateTime={response}>{response}</time>
-      </p>
+      <ThemeProvider theme={theme}>
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect={true}>
+            <WalletModalProvider>
+
+              <Home
+                  connection={connection}
+                  txTimeout={txTimeout}
+                  rpcHost={rpcHost}
+              />
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ client/src/Home.tsx
@@ -0,0 +1,322 @@
+import React, {useEffect, useState} from "react";
+import styled from "styled-components";
+import confetti from "canvas-confetti";
+import * as anchor from "@project-serum/anchor";
+import {LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
+import {useAnchorWallet} from "@solana/wallet-adapter-react";
+import {WalletMultiButton} from "@solana/wallet-adapter-react-ui";
+import {Snackbar, Paper, LinearProgress, Chip} from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+import {AlertState} from './utils.ts';
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:4001";
+const socket = socketIOClient(ENDPOINT);
+
+const WalletContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const WalletAmount = styled.div`
+  color: black;
+  width: auto;
+  padding: 5px 5px 5px 16px;
+  min-width: 48px;
+  min-height: auto;
+  border-radius: 22px;
+  background-color: white;
+  box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%), 0px 6px 10px 0px rgb(0 0 0 / 14%), 0px 1px 18px 0px rgb(0 0 0 / 12%);
+  box-sizing: border-box;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  font-weight: 500;
+  line-height: 1.75;
+  text-transform: uppercase;
+  border: 0;
+  margin: 0;
+  display: inline-flex;
+  outline: 0;
+  position: relative;
+  align-items: center;
+  user-select: none;
+  vertical-align: middle;
+  justify-content: flex-start;
+  gap: 10px;
+`;
+
+const Wallet = styled.ul`
+  flex: 0 0 auto;
+  margin: 0;
+  padding: 0;
+`;
+
+const ConnectButton = styled(WalletMultiButton)`
+  border-radius: 18px !important;
+  padding: 6px 16px;
+  background-color: #4E44CE;
+  margin: 0 auto;
+`;
+
+const NFT = styled(Paper)`
+  min-width: 400px;
+  padding: 5px 20px 20px 20px;
+  flex: 1 1 auto;
+  background-color: var(--card-background-color) !important;
+  color: var(--main-text-color) !important;
+`;
+const Des = styled(NFT)`
+  text-align: left;
+  padding-top: 0px;
+`;
+
+const Card = styled(Paper)`
+  display: inline-block;
+  background-color: var(--card-background-lighter-color) !important;
+  margin: 5px;
+  padding: 24px;
+`;
+
+const MintButtonContainer = styled.div`
+  button.MuiButton-contained:not(.MuiButton-containedPrimary).Mui-disabled {
+    color: #464646;
+  }
+
+  button.MuiButton-contained:not(.MuiButton-containedPrimary):hover,
+  button.MuiButton-contained:not(.MuiButton-containedPrimary):focus {
+    -webkit-animation: pulse 1s;
+    animation: pulse 1s;
+    box-shadow: 0 0 0 2em rgba(255, 255, 255, 0);
+  }
+
+  @-webkit-keyframes pulse {
+    0% {
+      box-shadow: 0 0 0 0 #ef8f6e;
+    }
+  }
+
+  @keyframes pulse {
+    0% {
+      box-shadow: 0 0 0 0 #ef8f6e;
+    }
+  }
+`;
+
+const Logo = styled.div`
+  flex: 0 0 auto;
+
+  img {
+    height: 60px;
+  }
+`;
+const Menu = styled.ul`
+  list-style: none;
+  display: inline-flex;
+  flex: 1 0 auto;
+
+  li {
+    margin: 0 12px;
+
+    a {
+      color: var(--main-text-color);
+      list-style-image: none;
+      list-style-position: outside;
+      list-style-type: none;
+      outline: none;
+      text-decoration: none;
+      text-size-adjust: 100%;
+      touch-action: manipulation;
+      transition: color 0.3s;
+      padding-bottom: 15px;
+
+      img {
+        max-height: 26px;
+      }
+    }
+
+    a:hover, a:active {
+      color: rgb(131, 146, 161);
+      border-bottom: 4px solid var(--title-text-color);
+    }
+
+  }
+`;
+
+const SolExplorerLink = styled.a`
+  color: var(--title-text-color);
+  border-bottom: 1px solid var(--title-text-color);
+  font-weight: bold;
+  list-style-image: none;
+  list-style-position: outside;
+  list-style-type: none;
+  outline: none;
+  text-decoration: none;
+  text-size-adjust: 100%;
+
+  :hover {
+    border-bottom: 2px solid var(--title-text-color);
+  }
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-right: 4%;
+  margin-left: 4%;
+  text-align: center;
+  justify-content: center;
+`;
+
+const MysteryContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 1 1 auto;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin-top: 20px;
+`;
+
+const DesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  gap: 20px;
+`;
+
+const Price = styled(Chip)`
+  position: absolute;
+  margin: 5px;
+  font-weight: bold;
+  font-size: 1em !important;
+`;
+
+const Image = styled.img`
+  height: 400px;
+  width: auto;
+  border-radius: 7px;
+  box-shadow: 5px 5px 40px 5px rgba(0,0,0,0.5);
+`;
+
+const BorderLinearProgress = styled(LinearProgress)`
+  margin: 20px auto;
+  width: 500px;
+  height: 10px !important;
+  border-radius: 30px;
+  border: 2px solid white;
+  box-shadow: 5px 3px 16px -3px rgba(0,0,0,0.5);
+  background-color:white !important;
+  
+  > div.MuiLinearProgress-barColorPrimary{
+    background-color:var(--second-accent) !important;
+  }
+
+  > div.MuiLinearProgress-bar1Determinate {
+    border-radius: 30px !important;
+    background-image: linear-gradient(270deg, rgba(255, 255, 255, 0.01), rgba(255, 255, 255, 0.5));
+  }
+`;
+
+const ShimmerTitle = styled.h1`
+  margin: 30px auto;
+  color: var(--main-text-color);
+`;
+
+const GoldTitle = styled.h2`
+  color: var(--title-text-color);
+`;
+
+const LogoAligner = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    max-height: 35px;
+    margin-right: 10px;
+  }
+`;
+
+export interface HomeProps {
+    connection: anchor.web3.Connection;
+    txTimeout: number;
+    rpcHost: string;
+}
+
+const Home = (props: HomeProps) => {
+    const [balance, setBalance] = useState<number>();
+    const [response, setResponse] = useState("");
+
+    const [alertState, setAlertState] = useState<AlertState>({
+        open: false,
+        message: "",
+        severity: undefined,
+    });
+
+    const wallet = useAnchorWallet();
+
+    function throwConfetti(): void {
+        confetti({
+            particleCount: 400,
+            spread: 70,
+            origin: {y: 0.6},
+        });
+    }
+
+    useEffect(() => {
+        (async () => {
+            if (wallet) {
+                const balance = await props.connection.getBalance(wallet.publicKey);
+                setBalance(balance / LAMPORTS_PER_SOL);
+
+                socket.emit("walletConnect", wallet.publicKey);
+            }
+        })();
+    }, [wallet, props.connection]);
+
+    useEffect(() => {
+        socket.on("FromAPI", (data: React.SetStateAction<string>) => {
+            setResponse(data);
+        });
+    }, []);
+
+
+    return (
+        <main>
+            <MainContainer>
+                <WalletContainer>
+                    <Logo><a href="http://localhost:3000/" target="_blank" rel="noopener noreferrer"><img alt=""
+                                                                                                          src="logo.png"/></a></Logo>
+                    <Menu />
+                    <Wallet>
+                        {wallet ?
+                            <WalletAmount>{(balance || 0).toLocaleString()} SOL<ConnectButton/></WalletAmount> :
+                            <ConnectButton>Connect Wallet</ConnectButton>}
+                    </Wallet>
+                </WalletContainer>
+                <br/>
+                <MysteryContainer>
+                    <DesContainer>
+                        <p>
+                            It's <time dateTime={response}>{response}</time>
+                        </p>
+                    </DesContainer>
+                </MysteryContainer>
+            </MainContainer>
+            <Snackbar
+                open={alertState.open}
+                autoHideDuration={6000}
+                onClose={() => setAlertState({...alertState, open: false})}
+            >
+                <Alert
+                    onClose={() => setAlertState({...alertState, open: false})}
+                    severity={alertState.severity}
+                >
+                    {alertState.message}
+                </Alert>
+            </Snackbar>
+        </main>
+    );
+};
+
+export default Home;
